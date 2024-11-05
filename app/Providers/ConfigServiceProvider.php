0c02545103b94353049a89b6e9bb295a65c8d29f
Polash Mahmud <polashmahmud@gmail.com>
2024-11-05 14:57:33 +0600

    class - 09

--- app/Providers/ConfigServiceProvider.php
+++ app/Providers/ConfigServiceProvider.php
@@ -2,27 +2,29 @@
 
 namespace App\Providers;
 
+use App\Config\Config;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use League\Container\ServiceProvider\BootableServiceProviderInterface;
 use Spatie\Ignition\Ignition;
 
-class AppServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+class ConfigServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
 {
     public function boot(): void
     {
-        // @todo only do this when debug is enabled
-        Ignition::make()->register();
+        //
     }
 
     public function register(): void
     {
-        //
+        $this->getContainer()->add(Config::class, function () {
+           return new Config();
+        });
     }
 
     public function provides(string $id): bool
     {
         $services = [
-            //
+            Config::class
         ];
 
         return in_array($id, $services);
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,21 @@
 <?php
 
+use App\Config\Config;
 use App\Core\App;
 use App\Core\Container;
+use App\Providers\AppServiceProvider;
+use App\Providers\ConfigServiceProvider;
 
 require '../vendor/autoload.php';
 
 $container = Container::getInstance();
 $container->delegate(new \League\Container\ReflectionContainer());
 
-$container->addServiceProvider(new \App\Providers\AppServiceProvider());
+$container->addServiceProvider(new ConfigServiceProvider());
+$container->addServiceProvider(new AppServiceProvider());
+
+var_dump($container->get(Config::class)->get('app.name'));
+die();
 
 $app = new App();
 // register routes
