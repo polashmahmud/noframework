f41ee69d9ca7566c2c9abb86c82a99583c5af20b
Polash Mahmud <polashmahmud@gmail.com>
2024-11-29 17:25:50 +0600

    class - 19

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Config\Config;
 use Laminas\Diactoros\Response;
 use Psr\Http\Message\ServerRequestInterface;
 
 
 class HomeController
 {
+    public function __construct(protected Config $config)
+    {
+    }
+
     public function __invoke(ServerRequestInterface $request)
     {
         $response = new Response();
 
-        $response->getBody()->write($request->getQueryParams()['name']);
+        $response->getBody()->write($this->config->get('app.name'));
 
         return $response;
     }
